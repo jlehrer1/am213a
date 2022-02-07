1e4b2baa52ca6a5c70a3fb7c8782fdf063106380
Julian Lehrer <jmlehrer@ucsc.edu>
2022-02-07 15:01:06 -0800

    testing

--- hw2/code/Driver_LinAl.f90
+++ hw2/code/Driver_LinAl.f90
@@ -1,37 +1,43 @@
 Program Driver_LinAl
 
-  use LinAl, only: mat, msize, nsize, readMat
+  use LinAl, only: mat, msize, nsize, readMat, prettyprint, n_norm, matrix_trace, dp, from_file, gaussian_elimination
 
   implicit none
   
-  character(len=100) :: myFileName
+  character(len=100) :: myFileName, file1, file2 
   integer :: i,j
-  real :: traceA
+  real (dp) :: traceA, normval 
+  real (dp), allocatable, dimension(:) :: vec
+  real (dp), allocatable, dimension(:, :) :: A, B
+  logical :: flag 
 
-  
   myFileName = 'Amat.dat'
+  call from_file(myFileName, A)
 
-  open(10,file=myFileName)
-  read(10,*) msize,nsize
-  close(10)
+  myFileName = 'Bmat.dat'
+  call from_file(myFileName, B)
 
-  allocate(mat(msize,nsize))
-  ! Always initialize with zeros
-  mat = 0.0
+  call prettyprint(A, 4, 4)
+  call prettyprint(B, 4, 6)
 
+  call matrix_trace(A, 4, traceA)
+
+  print *, 'The trace of A is', traceA
+
+  do i=1, 4
+    vec = A(:, i)
+    call n_norm(vec, 4, normval)
+    print *, 'The norm of column', i, 'is', normval 
+  end do 
   
-  
-  call readMat(myFileName)
+  call gaussian_elimination(A, B, flag, 4, 6)
 
-  do i = 1, msize
-     write(*,*) (mat(i,j) , j = 1, nsize )
-  end do
-  
+  print *, 'A after gaussian elimination is'
+  call prettyprint(A, 4, 4)
 
-  deallocate(mat)
+  ! print *, 'B after gaussian elimination is'
+  ! call prettyprint(B)
 
-
-
-
+  ! deallocate(mat)
 
 End Program Driver_LinAl
--- hw2/code/LinAl.f90
+++ hw2/code/LinAl.f90
@@ -1,7 +1,8 @@
 module LinAl
   implicit none
   integer, save :: msize, nsize
-  real, dimension(:,:), allocatable, save :: mat
+  integer, parameter :: dp = SELECTED_REAL_KIND(15) ! to use double precision throughout 
+  real (dp), dimension(:,:), allocatable, save :: mat
 
 contains
 
@@ -24,7 +25,6 @@ subroutine readMat(filename)
   ! then the next msize lines are the matrix entries. This matrix is found in Eq. 2.18 of the lecture note.
   ! Note that entries must be separated by a tab.
 
-
   open(10,file=filename)
 
   ! Read the matrix dimensions
@@ -39,37 +39,161 @@ subroutine readMat(filename)
   
 end subroutine readMat
 
+subroutine from_file(filename, matrix)
+  character(len=*) :: filename 
+  real (dp), intent(out), allocatable, dimension(:, :) :: matrix 
+  integer :: i, j, m, n
+
+  open(10,file=filename)
+  read(10,*) m, n
+  close(10)
+
+  open(10, file=filename)
+  read(10,*) i, j
+
+  allocate(matrix(m, n))
+
+  do i=1,m
+    read(10, *) (matrix(i, j), j=1,n)
+  end do
+
+end subroutine from_file 
+
 subroutine matrix_trace(A, m, trace)
   integer, intent(in) :: m
-  real, intent(in), dimension(m, m) :: A
-  real, intent(out) :: trace 
+  real (dp), intent(in), dimension(m, m) :: A
+  real (dp), intent(out) :: trace 
   integer :: i 
 
   do i=1,m
       trace = trace + A(i, i)
   end do
+
 end subroutine matrix_trace
 
 subroutine n_norm(vec, m, norm)
     integer, intent(in) :: m
-    real, intent(in), dimension(m) :: vec 
-    real, intent(out) :: norm 
+    real (dp), intent(in), dimension(m) :: vec 
+    real (dp), intent(out) :: norm 
 
     integer :: i
+    norm = 0.0
 
     do i=1,m
-      norm = norm + abs(vec(i)) ** 2
+      norm = norm + vec(i) ** 2
     end do
     norm = sqrt(norm)
 
 end subroutine n_norm
 
-subroutine print_matrix(A, m, n):
+subroutine prettyprint(A, m, n)
+  integer, intent(in) :: m, n
+  real (dp), intent(in), dimension(:, :) :: A
+  integer :: i, j
+
+  do i=1,m
+      write(*,"(100g15.5)") ( A(i,j), j=1,n )
+  enddo
+
+  ! write(*,"100g15.5") ( matrix(i,j), j=1,n )
+  ! do i=1,m 
+  !   print *, A(i, :)
+  ! end do 
+
+  print *, ''
+end subroutine prettyprint
+
+subroutine find_pivot(A, j, K, p, m)
+  ! Find the index K and pivot P such that 
+  ! p = max_{k=j...,msize} |a_{kj}|
+
+  ! This will be used in gaussian elimination with partial pivoting 
+  real (dp), intent(in), dimension(:, :) :: A 
+  integer, intent(in) :: j, m
+  
+  integer, intent(out) :: K 
+  real (dp), intent(out) :: p
+
+  integer :: i 
+  p = 0.0 
+  
+  do i=j,m  
+    if (abs(A(i, j)) > p) then 
+      p = abs(A(i, j))
+      K = i 
+    end if
+  end do
+
+end subroutine find_pivot 
+
+subroutine gaussian_elimination(A, B, flag, m, n)
+  real (dp), intent(inout), dimension(:, :) :: A, B 
+  logical, intent(out) :: flag 
+  integer, intent(in) :: m, n
+
+  real (dp) :: p, maxval
+  real (dp), allocatable, dimension(:) :: temp, r ! keeps vector when we're doing row swaps 
+  integer :: i, j, K
+
+  allocate(temp(n))
+  allocate(r(n))
+
+  temp = 0.0
+  r = 0.0 
+  flag = .false. !begin by assuming matrix is not singular
+
+  do j=1, m - 1 
+    ! find maximum pivot 
+    call find_pivot(A, j, K, p, m)
+    
+    ! if matrix is singular exit 
+    if (A(j, j) .eq. 0.0) then 
+      print *, 'ERROR: Matrix is singular'
+      flag = .true.
+      exit 
+    end if 
+
+    if (K .ne. j) then !swap the rows 
+      temp = A(K, :)
+      A(K, :) = A(j, :)
+      A(j, :) = temp 
+    end if 
+
+    do i=j+1, m 
+      A(i, :) = A(i, :) - A(i,j)*A(j, :)/A(j,j)
+      B(i, :) = B(i, :) - A(i, j)*B(j, :)/A(j, j)
+    end do 
+  end do
+  print *, 'hi'
+end subroutine gaussian_elimination
+
+subroutine lu_decomp(A, m, flag, s)
   integer, intent(in) :: m 
-  integer, intent(in) :: n 
-  real, intent(in) :: A(m, n)
+  real (dp), intent(in), dimension(m, m) :: A 
 
+  logical, intent(out) :: flag 
+  integer, intent(out), dimension(m) :: s 
 
-end subroutine print_matrix
+end subroutine lu_decomp
+
+subroutine backsolve(U, B, X)
+  real (dp), intent(in), dimension(:, :) :: U, B
+  real (dp), intent(out), dimension(:, :) :: X
+  real (dp) :: sum 
+  integer :: i, k, j
+
+  x = 0.0
+
+  ! Instead of Ux = b, we have UX = B
+  do j=1, nsize
+    do i=msize-1, 1, -1 
+      sum = 0.0 
+      do k=i+1, msize 
+        sum = sum + U(i, k)*X(j, k)
+      end do 
+    end do 
+    X(i, j) = B(i, j) / U(i, i)
+  end do
+end subroutine backsolve
 
 end module LinAl
